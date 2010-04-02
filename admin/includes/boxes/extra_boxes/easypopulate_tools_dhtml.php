63dc4d216c3c116663b188e13d0732ce65f9c7c1
John Robeson <johnny@localmomentum.net>
2010-04-02 13:20:15 -0400

    drop the english definitions for BOX_TOOLS_EASYPOPULATE while still allowing it to be overridden per language

--- admin/includes/boxes/extra_boxes/easypopulate_tools_dhtml.php
+++ admin/includes/boxes/extra_boxes/easypopulate_tools_dhtml.php
@@ -2,10 +2,19 @@
 /**
  * EasyPopulate admin menu link
  *
+ * If you want to provide a language specific defintion of BOX_TOOLS_EASYPOPULATE
+ * create a file in
+ * admin/includes/languages/$lang/extra_definitions/easypopulate.php like this:
+ * <?php define('BOX_TOOLS_EASYPOPULATE', 'Your Definition'); ?>
+ *
  * @package easypopulate
  * @author langer
  * @copyright 2003
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Publice License (v2 only)
  */
-$za_contents[] = array('text' => BOX_TOOLS_EASYPOPULATE, 'link' => zen_href_link('easypopulate.php', '', 'NONSSL'));
+if (!defined('BOX_TOOLS_EASYPOPULATE')) {
+	define('BOX_TOOLS_EASYPOPULATE', 'Easy Populate');
+}
+$za_contents[] = array('text' => BOX_TOOLS_EASYPOPULATE,
+'link' => zen_href_link('easypopulate.php', '', 'NONSSL'));
 ?>
