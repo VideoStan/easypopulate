015d9020dd1607bb9a81a72ab8502e70a3e4785f
John Robeson <johnny@localmomentum.net>
2010-04-05 13:39:14 -0400

    check for easypopulate.php in DIR_FS_ADMIN before adding the link to the admin menu entries in extra_boxes

--- admin/includes/boxes/extra_boxes/easypopulate_tools_dhtml.php
+++ admin/includes/boxes/extra_boxes/easypopulate_tools_dhtml.php
@@ -12,9 +12,11 @@
  * @copyright 2003
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Publice License (v2 only)
  */
-if (!defined('BOX_TOOLS_EASYPOPULATE')) {
-	define('BOX_TOOLS_EASYPOPULATE', 'Easy Populate');
+if (file_exists(DIR_FS_ADMIN . 'easypopulate.php')) {
+	if (!defined('BOX_TOOLS_EASYPOPULATE')) {
+		define('BOX_TOOLS_EASYPOPULATE', 'Easy Populate');
+	}
+	$za_contents[] = array('text' => BOX_TOOLS_EASYPOPULATE,
+	'link' => zen_href_link('easypopulate.php', '', 'NONSSL'));
 }
-$za_contents[] = array('text' => BOX_TOOLS_EASYPOPULATE,
-'link' => zen_href_link('easypopulate.php', '', 'NONSSL'));
 ?>
