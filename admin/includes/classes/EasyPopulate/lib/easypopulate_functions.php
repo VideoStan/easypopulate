5abee7bcf7035048cc34afbcfd7c04e12c73f949
John Robeson <johnny@localmomentum.net>
2010-10-08 14:19:51 -0400

    add new handler field, and and put a unique index on name field

--- admin/includes/classes/EasyPopulate/lib/easypopulate_functions.php
+++ admin/includes/classes/EasyPopulate/lib/easypopulate_functions.php
@@ -323,10 +323,12 @@ function install_easypopulate()
 	$query = 'CREATE TABLE IF NOT EXISTS ' . TABLE_EASYPOPULATE_FEEDS . ' (
 				id int(3) NOT NULL AUTO_INCREMENT,
 				name varchar(64),
+				handler varchar(64),
 				config text,
 				last_run_data text,
 				created datetime,
 				modified datetime,
+				UNIQUE KEY (name),
 				PRIMARY KEY (id)
 				)';
 	$db->Execute($query);
