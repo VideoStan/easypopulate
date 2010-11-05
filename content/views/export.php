2c126a0c596196a77a34a2abfc803d3a373a4ee6
John Robeson <johnny@localmomentum.net>
2010-11-05 14:54:12 -0400

    use zenmagick widgets and $request->url() in export page

--- content/views/export.php
+++ content/views/export.php
@@ -1,34 +1,19 @@
-<?php if (!empty($output['errors'])) { ?>
-	<p>Errors:</p>
-	<?php foreach ($output['errors'] as $error) { ?>
-		<p class="fail"><?php echo $error; ?></p>
-	<?php } ?>
-<?php } ?>
-<?php echo zen_draw_form('custom', 'easypopulate.php', 'id="custom"', 'get'); ?>
-	<!--  <form ENCTYPE="multipart/form-data" ACTION="easypopulate.php?download=stream&dltype=full" METHOD="POST"> -->
+<?php $admin2->title() ?>
+<form action="<?php echo $request->url('export'); ?>" name="custom" id="custom">
 	<div align="left">
-	<?php
-		$manufacturers_array = array();
-		$manufacturers_array[] = array( "id" => '', 'text' => "Manufacturers" );
-		$manufacturers_query = mysql_query("SELECT manufacturers_id, manufacturers_name FROM " . TABLE_MANUFACTURERS . " ORDER BY manufacturers_name");
-		while ($manufacturers = mysql_fetch_array($manufacturers_query)) {
-			$manufacturers_array[] = array( "id" => $manufacturers['manufacturers_id'], 'text' => $manufacturers['manufacturers_name'] );
-		}
-		$status_array = array(array( "id" => '1', 'text' => "status" ),array( "id" => '1', 'text' => "active" ),array( "id" => '0', 'text' => "inactive" ));
-		echo "Filter Complete Download by: " . zen_draw_pull_down_menu('ep_category_filter', array_merge(array( 0 => array( "id" => '', 'text' => "Categories" )), zen_get_category_tree()));
-		echo ' ' . zen_draw_pull_down_menu('ep_manufacturer_filter', $manufacturers_array) . ' ';
-		echo ' ' . zen_draw_pull_down_menu('ep_status_filter', $status_array) . ' ';
-		$download_array = array(array( "id" => 'download', 'text' => "download" ),array( "id" => 'stream', 'text' => "stream" ),array( "id" => 'tempfile', 'text' => "tempfile" ));
-		echo ' ' . zen_draw_pull_down_menu('download', $download_array) . ' ';
-
-		echo zen_draw_input_field('format', 'full', ' style="padding: 0px"', false, 'submit');
-	?>
+    	Filter Complete Download by: 
+		<?php echo $this->widget('ManufacturerSelectFormWidget#title=None&options=0= --- ','ep_manufacturer_filter'); ?> 
+		<?php echo $this->widget('SelectFormWidget#title=None&options=1=active&0=inactive','ep_status_filter'); ?> 
+		<?php echo $this->widget('SelectFormWidget#title=None&options=stream&tempfile','download'); ?> 
+		<?php // @todo <johnny> implement this as widget: zen_draw_pull_down_menu('ep_category_filter', array_merge(array( 0 => array( "id" => '', 'text' => "Categories" )), zen_get_category_tree())); ?>
+		<input type="submit" name="format" value="full" />
 	</div>
 </form>
 
-<b>Download Easy Populate Files</b>
+<strong>Download Easy Populate Files</strong>
 <?php
 // Add your custom fields here
+// @todo allow custom file output types similiar to how import handler configs work
 $ep_exports = array();
 $ep_exports['full'] = 'Complete';
 $ep_exports['priceqty'] = 'Model/Price/Qty';
@@ -40,20 +25,19 @@ $ep_exports['attrib_basic'] = 'Basic Products Attributes (multi-line)';
 $ep_exports['options'] = 'Attribute Options Names';
 $ep_exports['values'] = 'Attribute Options Values';
 $ep_exports['optionvalues'] = 'Attribute Options-Names-to-Values';
-$ep_exports['froogle'] = 'Froogle';
 ?>
 <table>
 	<thead>
 		<tr>
 			<th>Download</th>
-			<th>Create in Temp dir (<?php echo $tempdir ?>)</th>
+			<th>Create in Temp dir (<?php echo $temp_dir ?>)</th>
 		</tr>
 	</thead>
 	<tbody>
 	<?php foreach($ep_exports as $key => $value) { ?>
 		<tr>
-			<td><a href="/admin/easypopulate.php/export/<?php echo $key ?>/stream"><?php echo $value ?></a></td>
-			<td><a href="/admin/easypopulate.php/export/<?php echo $key ?>/tempfile"><?php echo $value ?></a></td>
+			<td><a href="<?php echo $request->url('export', "format=$key&download=stream"); ?>"><?php echo $value ?></a></td>
+			<td><a href="<?php echo $request->url('export', "format=$key&download=tempfile"); ?>"><?php echo $value ?></a></td>
 		</tr>
 	<?php } ?>
 	</tbody>
